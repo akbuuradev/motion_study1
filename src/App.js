191e425e85c8cf491bb7d2cdb15a23f1417f0523
akbuuradev <akbuurakudainazarkyzy@gmail.com>
2023-08-12 16:30:02 +0600

    home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.scss';
 import Header from "./components/Header/header";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+        <Routes>
+            <Route/>
+        </Routes>
     </div>
   );
 }
