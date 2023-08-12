c660952f1a5a3ce4eab542c06892f4ad355f1b8e
akbuuradev <akbuurakudainazarkyzy@gmail.com>
2023-08-12 15:39:27 +0600

    header

---
+++ src/components/Header/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div id="header">
+            <div className="container">
+                <div color="header">
+                    Hello
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
