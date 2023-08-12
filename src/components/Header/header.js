c8650dc7ce1da46dce96b5790b83d6915589e819
askat123 <asylbekuuluaskat11@gmail.com>
2023-08-12 16:59:05 +0600

    remove

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -36,7 +36,7 @@ const Header = () => {
                                    <span className="sr-only">Search icon</span>
                                </div>
                                <input type="text" id="search-navbar"
-                                      className="block w-full p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                      className="block w-full p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500  dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                       placeholder="Search..."/>
                            </div>
                            <select className="header--nav__select">
