18802edaf70f866ee1cec894e2b1590b6324f0a8
askat123 <asylbekuuluaskat11@gmail.com>
2023-08-13 19:24:28 +0600

    askat

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import Header from "./components/Header/header";
 import {Route, Routes} from "react-router-dom";
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
         <Routes>
             <Route/>
         </Routes>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Hero from '../Hero/hero';
+import BestFrom from '../BestFrom/BestFrom';
+import Countries from '../pages/Countries/Countries';
 
 const Home = () => {
     return (
         <div>
-            
+            <Hero/>
+            <BestFrom/>
+            <Countries/>
         </div>
     );
 };
