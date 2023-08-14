6b981b20ce2f003c1c838c08f3c9cbbf0f3a770e
akbuuradev <116719560+akbuuradev@users.noreply.github.com>
2023-08-14 16:43:14 +0600

    ertyu (#6)

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,20 @@
-
 import React from 'react';
 import Hero from '../Hero/hero';
 import BestFrom from '../BestFrom/BestFrom';
 import Countries from '../pages/Countries/Countries';
 import Application from "../Application/application.js";
-  import Exams from "../Exams/Exams";
+import Exams from "../Exams/Exams";
 import Slider from "../Slider/Slider";
+import Study from "../Study/study";
 
 const Home = () => {
     return (
         <div>
             <Hero/>
             <BestFrom/>
-            <Countries/>
+            <Exams/>
+            <Slider/>
             <Application/>
-      <Exams />
-      <Slider />
         </div>
     );
 };
---
+++ src/components/StudyAbb/studyAbb.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Study from "../Study/study";
+
+const StudyAbb = () => {
+    return (
+        <div>
+            <Study/>
+        </div>
+    );
+};
+
+export default StudyAbb;
