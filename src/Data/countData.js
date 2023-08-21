317810b249178515bca50897c29a28c431f047c9
askat123 <asylbekuuluaskat11@gmail.com>
2023-08-21 15:17:53 +0600

    askatdata

---
+++ src/Data/countData.js
@@ -0,0 +1,63 @@
+export const countries = [
+    {
+      id: 1,
+      name: "America",
+      flagImage: "../images/america.png", 
+    },
+    {
+      id: 2,
+      name: "Great Britain",
+      flagImage: "../images/greatBritan.png",
+    },
+    {
+      id: 3,
+      name: "Austria",
+      flagImage: "../images/ausrtra.png",
+    },
+    {
+      id: 4,
+      name: "Germany",
+      flagImage: "../images/germany.png",
+    },
+    {
+      id: 5,
+      name: "Holland",
+      flagImage: "../images/holland.png",
+    },
+    {
+      id: 6,
+      name: "Ireland",
+      flagImage: "../images/Ireland.png",
+    },
+    {
+      id: 7,
+      name: "Spain",
+      flagImage: "../images/Spain.png",
+    },
+    {
+      id: 8,
+      name: "Italy",
+      flagImage: "../images/Italy.png",
+    },
+    {
+      id: 9,
+      name: "Canada",
+      flagImage: "../images/canada.png",
+    },
+    {
+      id: 10,
+      name: "Cyprus",
+      flagImage: "../images/cyprus.png",
+    },
+    {
+      id: 11,
+      name: "China",
+      flagImage: "../images/china.png",
+    },
+    {
+      id: 12,
+      name: "Latvia",
+      flagImage: "../images/latvia.png",
+    },
+  ];
+  
